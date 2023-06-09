c151e7beaf5b7c541f6cfd69495a83784cbed041
dcmdev1979 <daniel@minervaconsultora.com>
2023-06-09 11:14:41 -0300

    usando node 18.5

--- firebase.js
+++ firebase.js
@@ -1,27 +1,52 @@
-//IMPORTAR VARIABLES DE ENTORNO
-
 import dotenv from "dotenv";
+import admin from 'firebase-admin';
+import { initializeApp } from 'firebase-admin/app';
+
 dotenv.config();
 
-//CONECTARSE A FIRESTORE
-
-import admin from 'firebase-admin';
-import { initializeApp, applicationDefault } from 'firebase-admin/app';
-
-
-
-initializeApp({
-  credential: applicationDefault(),
+const firebaseConfig = {
   projectId: 'tributapp-rg',
-});
+};
 
-if (!admin.firestore) {
-    console.log('El cliente de Firebase no se inicializó correctamente.');
-  } else {
-    console.log('El cliente de Firebase se ha inicializado correctamente.');
-  }
-  
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+  firebaseConfig.credential = admin.credential.cert(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+} else {
+  firebaseConfig.credential = admin.credential.applicationDefault();
+}
+
+initializeApp(firebaseConfig);
 
 const db = admin.firestore();
 
 export default db;
+
+
+
+
+// //IMPORTAR VARIABLES DE ENTORNO
+
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// //CONECTARSE A FIRESTORE
+
+// import admin from 'firebase-admin';
+// import { initializeApp, applicationDefault } from 'firebase-admin/app';
+
+
+
+// initializeApp({
+//   credential: applicationDefault(),
+//   projectId: 'tributapp-rg',
+// });
+
+// if (!admin.firestore) {
+//     console.log('El cliente de Firebase no se inicializó correctamente.');
+//   } else {
+//     console.log('El cliente de Firebase se ha inicializado correctamente.');
+//   }
+  
+
+// const db = admin.firestore();
+
+// export default db;
